c65d1b2104f7550e2cadfa245d9c4f08f319989a
stephanosiame <siamelstephano@gmail.com>
2024-12-13 22:38:28 +0300

    add file grade.py

---
+++ grade.py
@@ -0,0 +1,9 @@
+# Looping through some numbers... are they grades? Maybe?
+grades = [85, 92, 78, 99, 65]
+for score in grades:
+  # High numbers = good? 
+  if score >= 90:
+    print(score, "is an A.")
+  # Maybe lower numbers are okay? 
+  elif score >= 80:
+    print(score, "is a B.")
